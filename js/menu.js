e0d53e042bbee897d42ac034d3cdda3084d676d3
Martin Ratchford <ratchfordm@etown.edu>
2024-09-25 10:38:29 -0400

    added folder for css, and js

---
+++ js/menu.js
@@ -0,0 +1,38 @@
+"use strict";
+
+(function(){
+    window.addEventListener("load", init);
+
+    function init() {
+
+    }
+
+
+
+    function settings() {
+        // redirect to settings
+    }
+
+    function im_ready() {
+ // redirect to play
+    }
+
+    function character() {
+ // redirect to character selection
+    }
+
+    function achievement() {
+ // redirect to achievements page
+    }
+
+    function high_score() {
+ // redirect to high score page
+    }
+    
+
+
+
+
+
+
+})();
