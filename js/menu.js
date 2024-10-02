633c3c194d0c10af7f3b15b4b369852a590e4774
Martin Ratchford <ratchfordm@etown.edu>
2024-10-01 21:37:29 -0400

    added folder for menu items

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,26 @@
 "use strict";
 
 (function(){
-    window.addEventListener("load", init);
+   window.addEventListener("load", init);
+   
 
-    function init() {
 
-    }
+   function init() {
+      window.addEventListener("click", character);
+      window.addEventListener("click", im_ready);
 
-    function im_ready() {
+   }
+
+   function im_ready() {
  // redirect to play
-    }
+   let url_ready = 'im_ready.html';
+   window.location.href = url_ready;
+   }
 
-    function character() {
+   function character() {
  // redirect to character selection
-    }
+   let url_char = 'character.html';
+   window.location.href = url_char;      
+   }
     
 })();
