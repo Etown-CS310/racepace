5504ce82dc8c1de001e1d523780528a99d11d6b9
Asher Wayde <ashbwayde@gmail.com>
2024-10-02 12:11:27 -0400

    Adding menu stuff + .gitignore

---
+++ js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+(function(){
+
+window.addEventListener('load',init);
+
+function init(){
+    let loginButton=document.getElementById("loginButton");
+    loginButton.addEventListener("click",login);
+}
+
+function login(){
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+    if(username=="root" & password==""){
+        location.href ="menu.html";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}());
