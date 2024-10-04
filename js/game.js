ece0c87d3c414994ea2670bfe4bbc1330c95ed40
Pitlug <pitlug_greymore@pm.me>
2024-10-04 11:45:39 -0400

    Update game.js

---
+++ js/game.js
@@ -0,0 +1,31 @@
+
+//board
+let board;
+let boardWidth = 750;
+let bordWeight = 250;
+let context;
+
+//dino
+let dinoWidth = 88;
+let dinoHeight = 94;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+let dino = {
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth,
+    height : dinoHeight
+}
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d");
+
+    //darw intial dinosaur
+    context.fillStyle="green";
+    context.fillRect(dino.x, dino.y, dino.width, dino.height);
+}
