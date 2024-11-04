893f33005d0eda348a41d95780a3f6d7f15087f0
Martin Ratchford <ratchfordm@etown.edu>
2024-11-04 11:28:00 -0500

    character selection

---
+++ webdocs/js/character_select.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function () {
+
+
+
+    function init() {
+        makeRequest();
+        id("selectChar").addEventListener('click', function(){makeRequest();});
+
+    }
+
+    
+})
