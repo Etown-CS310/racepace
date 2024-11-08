c173783b764690d2c76492a5b250756007cafb92
Asher Wayde <ashbwayde@gmail.com>
2024-11-08 12:19:15 -0500

    Updating Database structure

--- racepace.sql
+++ racepace.sql
@@ -1,11 +1,27 @@
 drop table if exists users;
+drop table if exists characters;
 
 create table users(
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
     password TEXT,
-    current_character TEXT DEFAULT "jakob"
+    current_character INTEGER DEFAULT 0
 );
 
+create table characters(
+    character_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT UNIQUE
+);
+insert into characters(character_name)
+values('jakob');
+insert into characters(character_name)
+values('cole');
+insert into characters(character_name)
+values('Mo');
+insert into characters(character_name)
+values('Eliud');
+insert into characters(character_name)
+values('Grant');
+
 insert into users(username,password,current_character)
-values ('root','','jakob');
+values ('root','',0);
